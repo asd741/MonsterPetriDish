6658432681d4cb5a243f3b71a4ee9261022b0637
asd741 <a22833593@gmail.com>
2019-02-13 23:03:40 +0800

    '架構修復完成'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Banner from '../src/components/banner';
+import Monster from '../src/components/monster';
+export default　class App extends Component {
+  render() {
+    return (
+      <>
+        <Banner/>
+        {/* <Monster/> */}
+      </>
+    );
+  }
+}
